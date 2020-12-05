2322761db48500a64897be147fc3c2790fc04191
sadgabriel <66413143+sadgabriel@users.noreply.github.com>
2020-12-06 00:20:38 +0900

    saveloader

--- SaveLoader.cpp
+++ SaveLoader.cpp
@@ -1,1 +1,19 @@
 #include "SaveLoader.h"
+
+
+void SaveLoader::save(std::string filename, Sheet& mysheet) {
+    std::ofstream output(filename, std::ios::out);
+
+    for (int page_idx = 0; page_idx < 30; page_idx++) {
+        for (int cursor_idx = 0; cursor_idx < 48; cursor_idx++) {
+            Note mynote = mysheet.getNote(cursor_idx, page_idx);
+            output << mynote.freq << ':' << mynote.length << std::endl;
+        }
+    }
+}
+
+void SaveLoader::load(std::string filename, Sheet& mysheet) {
+    std::ifstream input(filename, std::ios::in);
+
+
+}
--- SaveLoader.h
+++ SaveLoader.h
@@ -1,5 +1,16 @@
 #pragma once
-class SaveLoader
-{
+
+#include <string>
+#include <fstream>
+
+#include "Sheet.h"
+
+class SaveLoader {
+public:
+    void save(std::string filename, Sheet& mysheet);
+private:
+    void load(std::string filename, Sheet& mysheet);
+
+
 };
 
